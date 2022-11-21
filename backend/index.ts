52d313dd0fa1a44288906bc92a741a84044b7ac5
Phil DeSousa <phil.desousa@gmail.com>
2022-11-21 14:41:22 -0500

    preparing for deployment

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import getRandomWords from './handlers/getRandomWords';
+import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ status: 200, message: 'OK' });
